27726c2f7df0cf05f0ede7cc8759afa5d8e5a305
sghenzy <benedetta.bigliardo@gmail.com>
2024-10-10 20:42:10 +0200

    Update demo2.js

--- js/demo2.js
+++ js/demo2.js
@@ -26,13 +26,13 @@ let sketch = new Sketch({
       float aspectRatio = resolution.x / resolution.y;
       float imageAspectRatio = resolution.z / resolution.w;
 
-      // Riduciamo leggermente il fattore di ridimensionamento per evitare troppo zoom
+      // Riduciamo ulteriormente il fattore di ridimensionamento (zoom ancora più basso)
       if (aspectRatio > imageAspectRatio) {
         // Se la finestra è più larga rispetto al video, ridimensioniamo in altezza (con meno zoom)
-        newUV.y = newUV.y * imageAspectRatio / aspectRatio * 0.95 + (1.0 - imageAspectRatio / aspectRatio * 0.95) * 0.5;
+        newUV.y = newUV.y * imageAspectRatio / aspectRatio * 0.85 + (1.0 - imageAspectRatio / aspectRatio * 0.85) * 0.5;
       } else {
         // Se la finestra è più alta rispetto al video, ridimensioniamo in larghezza (con meno zoom)
-        newUV.x = newUV.x * aspectRatio / imageAspectRatio * 0.95 + (1.0 - aspectRatio / imageAspectRatio * 0.95) * 0.5;
+        newUV.x = newUV.x * aspectRatio / imageAspectRatio * 0.85 + (1.0 - aspectRatio / imageAspectRatio * 0.85) * 0.5;
       }
       
       // Uso di smoothstep per creare una transizione più fluida
@@ -49,3 +49,4 @@ let sketch = new Sketch({
     }
   `
 });
+
