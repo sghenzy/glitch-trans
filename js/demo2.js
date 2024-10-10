95dd5dd6e549fb2bead23942a04aa15618777601
sghenzy <benedetta.bigliardo@gmail.com>
2024-10-10 19:32:14 +0200

    Update demo2.js

--- js/demo2.js
+++ js/demo2.js
@@ -8,41 +8,32 @@ let sketch = new Sketch({
   fragment: `
     uniform float time;
     uniform float progress;
-    uniform float width;
-    uniform float scaleX;
-    uniform float scaleY;
-    uniform float transition;
-    uniform float radius;
-    uniform float swipe;
     uniform sampler2D texture1;
     uniform sampler2D texture2;
-    uniform sampler2D displacement;
     uniform vec4 resolution;
 
     varying vec2 vUv;
 
-    vec2 mirrored(vec2 v) {
-      vec2 m = mod(v, 2.);
-      return mix(m, 2.0 - m, step(1.0, m));
-    }
-
     void main() {
-      // Mappa le coordinate UV in modo da coprire tutta la finestra senza distorsioni
+      // Corregge le coordinate UV per centrare il video senza distorsioni
       vec2 newUV = (vUv - vec2(0.5)) * resolution.zw + vec2(0.5);
       
-      // Prendi la mappa di distorsione
-      vec4 noise = texture2D(displacement, mirrored(newUV + time * 0.04));
-
-      // Modifica il progresso della transizione con la distorsione
-      float prog = progress * 0.8 - 0.05 + noise.g * 0.06;
-      float intpl = pow(abs(smoothstep(0.0, 1.0, (prog * 2.0 - vUv.x + 0.5))), 10.0);
-
-      // Recupera i colori dai due video
-      vec4 t1 = texture2D(texture1, (newUV - 0.5) * (1.0 - intpl) + 0.5);
-      vec4 t2 = texture2D(texture2, (newUV - 0.5) * intpl + 0.5);
+      // Variabili per la progressione
+      vec2 p = newUV;
+      float x = progress;
       
-      // Miscelazione tra i due video
-      gl_FragColor = mix(t1, t2, intpl);
+      // Uso di smoothstep per creare una transizione più fluida
+      x = smoothstep(0.0, 1.0, (x * 2.0 + p.y - 1.0));
+      
+      // Interpolazione tra i due video
+      vec4 f = mix(
+        texture2D(texture1, (p - 0.5) * (1.0 - x) + 0.5), 
+        texture2D(texture2, (p - 0.5) * x + 0.5), 
+        x
+      );
+      
+      gl_FragColor = f;
     }
   `
 });
+
