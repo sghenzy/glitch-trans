f3de9d2f4c2052204041557ef6e4e45f9d5567f9
sghenzy <benedetta.bigliardo@gmail.com>
2024-10-10 18:25:27 +0200

    Update demo2.js

--- js/demo2.js
+++ js/demo2.js
@@ -1,26 +1,38 @@
 let sketch = new Sketch({
-	duration: 1.5,
-	debug: false,
-	easing: 'easeOut',
-	uniforms: {
-		width: {value: 0.5, type:'f', min:0, max:10},
-	},
-	fragment: `
-		uniform float time;
-		uniform float progress;
-		uniform sampler2D texture1;
-		uniform sampler2D texture2;
-		uniform vec4 resolution;
+  duration: 1.5,
+  debug: false,
+  easing: 'easeOut',
+  uniforms: {
+    width: { value: 0.5, type: 'f', min: 0, max: 10 },
+  },
+  fragment: `
+    uniform float time;
+    uniform float progress;
+    uniform sampler2D texture1;
+    uniform sampler2D texture2;
+    uniform vec4 resolution;
 
-		varying vec2 vUv;
-		varying vec4 vPosition;
+    varying vec2 vUv;
+    varying vec4 vPosition;
 
-		void main() {
-    			vec2 newUV = (vUv - vec2(0.5)) * resolution.zw + vec2(0.5);
+    void main() {
+      vec2 newUV = (vUv - vec2(0.5)) * resolution.zw + vec2(0.5);
 
-    			// Renderizza solo il primo video senza transizione per testare
-    			vec4 color1 = texture2D(texture1, newUV);
-    			gl_FragColor = color1;
-		}
-	`
+      // Applica l'effetto glitch solo durante la transizione (quando progress è tra 0 e 1)
+      if (progress < 1.0 && progress > 0.0) {
+        // Distorsione glitch temporanea
+        newUV.x += sin(newUV.y * 10.0 + time * 5.0) * 0.05;
+        newUV.y += cos(newUV.x * 10.0 + time * 5.0) * 0.05;
+      }
+
+      // Misceliamo i due video in base al progresso della transizione
+      vec4 color1 = texture2D(texture1, newUV);
+      vec4 color2 = texture2D(texture2, newUV);
+
+      // Interpolazione tra i due video in base al progresso della transizione
+      vec4 finalColor = mix(color1, color2, progress);
+
+      gl_FragColor = finalColor;
+    }
+  `
 });
