7de2a80488eaf8fdf7d11ceea726c4a68b34411a
sghenzy <benedetta.bigliardo@gmail.com>
2024-10-10 16:18:08 +0200

    Update demo2.js

--- js/demo2.js
+++ js/demo2.js
@@ -1,7 +1,28 @@
-void main() {
-    vec2 newUV = (vUv - vec2(0.5)) * resolution.zw + vec2(0.5);
+let sketch = new Sketch({
+	duration: 1.5,
+	debug: false,
+	easing: 'easeOut',
+	uniforms: {
+		width: { value: 0.5, type: 'f', min: 0, max: 10 },
+	},
+	fragment: `
+		uniform float time;
+		uniform float progress;
+		uniform sampler2D texture1;
+		uniform sampler2D texture2;
+		uniform vec4 resolution;
 
-    // Renderizza solo il primo video senza transizione per il test
-    vec4 color1 = texture2D(texture1, newUV);
-    gl_FragColor = color1;
-}
+		varying vec2 vUv;
+		varying vec4 vPosition;
+
+		void main() {
+    		vec2 newUV = (vUv - vec2(0.5)) * resolution.zw + vec2(0.5);
+
+    		// Renderizza solo il primo video senza transizione per il test
+    		vec4 color1 = texture2D(texture1, newUV);
+    		gl_FragColor = color1;
+		}
+
+	`
+});
+
